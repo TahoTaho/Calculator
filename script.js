0a3ae8cbdb74878abf44ee561c95d5f61b1a17d3
Kyle Salvador <kizutsuitemotachiagaru@gmail.com>
2023-07-26 16:35:47 +0800

    calculator.js change name to script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+let userInput1 = userInput;
+let userInput2 = userInput;
+let selectedOperator = userInput;
+
+function operate(num1, num2, operator) {
+    
+}
+
+function add(addend1, addend2) {
+    const sum = addend1 + addend2;
+    return sum;
+}
+
+function subtract(minuend, subtrahend) {
+    const difference = minuend - subtrahend;
+    return difference;
+}
+
+function multiply(multiplicand, multiplier) {
+    const product = multiplicand * multiplier;
+    return product;
+}
+
+function divide(dividend, divisor) {
+    const quotient = dividend / divisor;
+    return quotient;
+}
+
+
+
