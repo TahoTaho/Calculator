31f16653f6ae14e29cc3c530615e87f2249619fc
Kyle Salvador <kizutsuitemotachiagaru@gmail.com>
2023-07-31 14:57:42 +0800

    If an operator key is pressed, the next input will replace the current one, instead of clearing the screen after an operator key is pressed

--- script.js
+++ script.js
@@ -1,12 +1,71 @@
 const display = document.querySelector('input.calculator-screen');
-const clearButton = document.querySelector('#clear');
-clearButton.addEventListener('click', clearScreen);
+const clearButton = document.getElementById('clear');
+const equalButton = document.getElementById('equal');
+const addButton = document.getElementById('add');
+const minusButton = document.getElementById('subtract');
 
+let input1;
+let input2;
+let selectedOperator;
+let buttonClicked = false;
+
+
+addButton.addEventListener('click', (e) => {
+    if (input1 != null && input2 === null) {
+        input2 = parseInt(display.value);
+        console.log('Input2: '+input2);
+        const result = operate(input1,input2,selectedOperator);
+        display.value = result;
+        input1 = result;
+    }
+    else {
+        input1 = parseInt(display.value);
+        console.log('Input1: ' + input1);
+        console.log('Input2: ' + input2);
+        selectedOperator = '+';
+    }
+    buttonClicked = true;
+    console.log('Button is clicked: ' + buttonClicked);
+
+});
+
+minusButton.addEventListener('click', () => {
+    if (input1 == null) {
+        input1 = parseInt(display.value);
+        console.log(input1);
+        console.log(input2);
+    }
+    else {
+        const result = operate(input1,input2,selectedOperator);
+        display.value = result;
+        input1 = result;
+        input2 = null;
+
+        console.log(input1);
+        console.log(input2);
+        console.log(operate(input1,input2,selectedOperator));
+    }
+    display.value = '';
+    selectedOperator = '-';
+})
+
+equalButton.addEventListener('click', () => {
+    if (input1 != null) {
+        input2 = parseInt(display.value);
+        console.log('I(=)Input2: ' + input2);
+        const result = operate(input1,input2,selectedOperator);
+        display.value = result;
+        input1 = result;
+        console.log('I(=)Input1: ' + input1)
+    }
+});
+
+
+clearButton.addEventListener('click', clearScreen);
 window.addEventListener("load", addButtonListener);
 
-
 function addButtonListener() {  
-    const buttons = document.querySelectorAll('button')
+    const buttons = document.querySelectorAll('button.calculator-numpad.number-key')
     buttons.forEach(button => {
         button.addEventListener('click', (e) => {
             getButtonValue(e.target);
@@ -14,10 +73,16 @@ function addButtonListener() {
     });
 };
 
-function getButtonValue (element) {
+function getButtonValue(element) {
     let clickedButtonValue = element.value;
     if (display.value === '0') {
         display.value = clickedButtonValue;
+    }
+    else if (buttonClicked === true) {
+        display.value = null;
+        buttonClicked = false;
+        console.log('Button is clicked: '+ buttonClicked)
+        display.value += clickedButtonValue;
     }
     else {
     display.value += clickedButtonValue;
@@ -26,10 +91,18 @@ function getButtonValue (element) {
 
 function clearScreen() {
     display.value = '0';
+    input1 = null;
+    input2 = null;
 }
 
 function operate(num1, num2, operator) {
-    
+    switch (operator) {
+        case '+': return add(num1, num2);
+        case '-': return subtract(num1, num2);
+        case '*': return multiply(num1, num2);
+        case '/': return divide(num1,num2);
+        default: return 'Operetor does not exist';
+    }
 }
 
 function add(addend1, addend2) {
